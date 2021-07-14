3a6d4cd53b3682264cfa72e9ac91b20f76ddc160
Dusan Vulic <dusanvulic11@gmail.com>
2021-07-14 21:02:33 +0200

    initial commit

---
+++ src/Tours.js
@@ -0,0 +1,21 @@
+import Tour from "./Tour";
+
+const Tours = ({tours, removeTour}) => {
+    return (
+
+       <section>
+           <div className="title">
+               <h2> Our tours </h2>
+               <div className="underline"></div>
+           </div>
+           <div>
+               { tours && tours.map((tour)=> {
+                 return <Tour key={tour.id} {...tour} removeTour={removeTour} > </Tour>;
+               })}
+           </div>
+       </section>
+      );
+}
+ 
+export default Tours;
+
